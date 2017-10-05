169876d5059444068f4034974d32105b2c7804e4
Bruno Azzi <bruno.azzi@icloud.com>
2017-10-04 23:09:15 -0300

    Add header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
+    HeaderComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
